d09375a00c4362cce2e18181f64e63dbd197fea0
Matt Woelk <matt.woelk@gmail.com>
2012-06-19 13:48:17 -0500

    Iris data now begins by spreading out from the middle.

--- iris.js
+++ iris.js
@@ -1,11 +1,17 @@
+//IDEAS:
+// - a 'keep axes rigid' button which keeps the upper and lower bounds on the graph constant, independent of what property is being shown.
+
 var MARGINS = {top: 2, right: 2, bottom: 2, left: 2}, // margins around the graph (percentages)
   xRange = d3.scale.linear().range([MARGINS.left + "%", (100 - MARGINS.right) + "%"]), // x range function (percentages)
-  yRange = d3.scale.linear().range([100 - MARGINS.top, MARGINS.bottom]), // y range function (percentages)
-  //rRange = d3.scale.linear().range([5, 20]); // radius range function - ensures the radius is between 5 and 20
+  yRange = d3.scale.linear().range([100 - MARGINS.top + "%", MARGINS.bottom + "%"]), // y range function (percentages)
   xAxis = d3.svg.axis().scale(xRange).tickSize(10).tickSubdivide(true),
   yAxis = d3.svg.axis().scale(yRange).tickSize(10).tickSubdivide(true).orient("right"),
   chart, //the chart where everything is drawn.
   drawingData, //the data which will be drawn.
+  chart_width = 600,
+  chart_height = 200,
+
+  //TODO: not yet used
   species= [
     "setosa",
     "versicolor",
@@ -20,17 +26,17 @@ var MARGINS = {top: 2, right: 2, bottom: 2, left: 2}, // margins around the grap
 
 chart = d3.select("#chart");
 
-chart.attr("width", "600px")
-  .attr("height", "200px");
+chart.attr("width", chart_width + "px")
+  .attr("height", chart_height + "px");
 
 d3.csv("iris.csv", function(data) {
   drawingData = data;
   d3.select("#chart").selectAll("circle")
       .data(data)
     .enter().append("circle")
-      .attr("r", function(d) { return d.seplen; })
-      .attr("cx", function(d) { return d.sepwid * 20; })
-      .attr("cy", function(d) { return 20*d.petlen; })
+      .attr("r", function(d) { return 5; })
+      .attr("cx", function(d) { return "50%"; })
+      .attr("cy", function(d) { return "50%"; })
       .attr("class", function(d) { return d.species + "_svg"; });
   redraw();
 });
@@ -40,31 +46,28 @@ d3.csv("iris.csv", function(data) {
 // this redraws the graph when forms are clicked
 function redraw () {
   console.log("redraw:");
-  // THIS WHOLE FUNCTION DOES NOT YET WORK //
   chart = d3.select("#chart");
 
-  //console.log(chart);
-
   var dataPoints = chart.selectAll("circle").data(drawingData), // select the data points and set their data
-    axes = getChosenAxes (); // object containing the axes we'd like to use (duration, inversions, etc.)
-
-  //console.log(dataPoints);
+      axes = getChosenAxes (); // object containing the axes we'd like to use (duration, inversions, etc.)
 
   xRange.domain([
     d3.min(drawingData, function (d) { return +d[axes.xAxis]; }),
     d3.max(drawingData, function (d) { return +d[axes.xAxis]; })
   ]);
+
   yRange.domain([
     d3.min(drawingData, function (d) { return +d[axes.yAxis]; }),
     d3.max(drawingData, function (d) { return +d[axes.yAxis]; })
   ]);
 
   // add new points if they're needed
-  //dataPoints.enter()
-    //.append("svg:circle")
-      //.attr("cx", 10)
-      //.attr("cy", 10)
-      //.style("fill", "#f0f"); // set fill colour from the colours array
+  dataPoints.enter()
+    .append("svg:circle")
+      .attr("cx", 10)
+      .attr("cr", 5)
+      .attr("cy", 10)
+      .style("fill", "#f0f"); // set fill colour from the colours array
 
   /*
   // the data domains or desired axes might have changed, so update them all
@@ -126,4 +129,3 @@ function getChosenFlowers () {
 // listen to the form fields changing
 document.getElementById("controls").addEventListener ("click", redraw, false);
 document.getElementById("controls").addEventListener ("keyup", redraw, false);
-
