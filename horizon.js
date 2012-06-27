dc52b28dbd709c3228740a90ec7de5b79d1ba17f
Matt Woelk <matt.woelk@gmail.com>
2012-06-27 12:14:09 -0500

    Horizon is now dynamic (fewer magic numbers)
    also, it uses a clipPath to do cropping, which is very good.

--- horizon.js
+++ horizon.js
@@ -1,38 +1,53 @@
-var data = [1, 5, 3, 7, 10, 0, 7, 8, 6, 3, 0, 1, 2, 7, 8, 2]; //TODO: This isn't actually mapping properly... maybe scales will fix it.
+var data = [1, 5, 10, 7, 10, 0, 7, 8, 6, 3, 0, 1, 2, 7, 8, 2];
 
-var chart = d3.select("#chart").append("svg");
+var chart = d3.select("#chart").append("svg:g");
+var height = 50;
+//var width = 800;
+var width = document.documentElement.clientWidth - 20;
 
-chart.attr("width", 1200)
-  .attr("height", 1600);
-
-// Make one array for each band, and store all arrays in a big array
-//var allArrays = [[], [], []];
 var numOfBands = 2;
 var maxValue = d3.max(data);
 var modValue = d3.max(data) / numOfBands; //TODO: use a d3 scale instead.
 
-var d3area1 = d3.svg.area()
-  .x(function (d, i) { return i * 20; })
-  .y1(function (d, i) { return 100 - (d * 10); })
-  .y0(100)
-  .interpolate("basis");
 
-//var marker5 = d3.svg.marker();
 //var colors = ["steelblue", "lightblue"];
 var colors = ["#F88", "#F44"];
 
-for (var i = 0; i < numOfBands; i++) {
-  chart.append("svg:path")
-    .attr("d", d3area1(data))
-    .style("stroke-width", 2)
-    .style("fill", colors[i])
-    .style("cursor", "help")
-    .attr("transform", "translate(0, " + (i - 1) * 50 + ")");
+var d3area1 = d3.svg.area()
+  .x(function (d, i) { return width / data.length * i; })
+  .y1(function (d, i) { return 100 - (d * 10); })
+  .y0(100)
+  .interpolate("cardinal");
 
-  chart.attr("width", data.length * 50).attr("height", 50);
-}
+d3.select("#chart")
+  .attr("width", width)
+  .attr("height", height);
+
+chart.insert("defs")
+  .append("clipPath")
+    .attr("id", "clip")
+  .append("rect")
+    .attr("width", width)
+    .attr("height", height); //height / 4 - 20);
+
+chart.attr("width", width + 100)
+//  .attr("clip-path", "url(#clip)")
+  .attr("height", height + 100);
+
+chart.selectAll("path")
+    .data(d3.range(numOfBands))
+  .enter().append("path")
+    .attr("fill", function (d, i) { return colors[i]; })
+    .style("stroke-width", 2)
+    .style("cursor", "help")
+    .attr("d", d3area1(data))
+    .attr("transform", function (d, i) {return "translate(0, " + (i - 1) * 50 + ")"; });
+
+
 
 
 //TODO: wrap this all up into a nice abstractable class thing :)
-//      - get rid of magic numbers
+//      - use scales instead of magic
 //      - make it work for negative values (might work already)
+//      Make the graph dynamic so that it sizes according to the screen
+//      - this might just mean setting it statically each time the screen is loaded or rotated.
