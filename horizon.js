07a5411da2d24a594fa6724d2e7e50949dee807c
Matt Woelk <matt.woelk@gmail.com>
2012-06-27 09:53:57 -0500

    Mild Horizon improvements.
    Just different data and colours.

--- horizon.js
+++ horizon.js
@@ -1,8 +1,8 @@
-var data = [1, 5, 3, 7, 10, 4, 7, 8, 6, 3, 0, 1];
+var data = [1, 5, 3, 7, 10, 0, 7, 8, 6, 3, 0, 1, 2, 7, 8, 2]; //TODO: This isn't actually mapping properly... maybe scales will fix it.
 
 var chart = d3.select("#chart").append("svg");
 
-chart.attr("width", 900)
+chart.attr("width", 1200)
   .attr("height", 1600);
 
 // Make one array for each band, and store all arrays in a big array
@@ -12,13 +12,14 @@ var maxValue = d3.max(data);
 var modValue = d3.max(data) / numOfBands; //TODO: use a d3 scale instead.
 
 var d3area1 = d3.svg.area()
-  .x(function (d, i) { return i * 30; })
+  .x(function (d, i) { return i * 20; })
   .y1(function (d, i) { return 100 - (d * 10); })
   .y0(100)
   .interpolate("basis");
 
 //var marker5 = d3.svg.marker();
-var colors = ["steelblue", "lightblue"];
+//var colors = ["steelblue", "lightblue"];
+var colors = ["#F88", "#F44"];
 
 for (var i = 0; i < numOfBands; i++) {
   chart.append("svg:path")
@@ -28,7 +29,7 @@ for (var i = 0; i < numOfBands; i++) {
     .style("cursor", "help")
     .attr("transform", "translate(0, " + (i - 1) * 50 + ")");
 
-  chart.attr("width", data.length * 30).attr("height", 50);
+  chart.attr("width", data.length * 50).attr("height", 50);
 }
 
 
