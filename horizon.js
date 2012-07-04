09db2fd86043e71a6bac5c080b028b070d0d4ce9
Matt Woelk <matt.woelk@gmail.com>
2012-07-04 09:25:20 -0500

    horizon multiple charts and width scrolling fix.

--- horizon.js
+++ horizon.js
@@ -1,5 +1,5 @@
 
-function coolChart() {
+function coolChart(whereToDrawIt) {
   var colors = ["#F88", "#F44"];
   //var colors = ["steelblue", "lightblue"];
   var numOfBands = 2;
@@ -7,7 +7,7 @@ function coolChart() {
   var width = document.documentElement.clientWidth - 20;
   var zeroPoint = 0;
 
-  var bandSize = 5; // maybe have a constant band size instead of setting the number of bands.
+  var bandSize = 5; // maybe have this constant band size instead of setting the number of bands.
 
   var upperBound = d3.max(data); //TODO: make use of these in scales.
   var lowerBound = d3.min(0, d3.min(data));
@@ -30,30 +30,31 @@ function coolChart() {
   //  .domain([d3.min(d3.min(data), zeroPoint), zeroPoint])
   //  .range([height * numOfBands, 0]);
 
+  var chart;
 
   var d3area1 = d3.svg.area()
     .x(function (d, i) { return xScale(i); })
     .y1(function (d, i) { return yScalePos(d); }) // height - (d * 10); })
-    .y0(height * numOfBands)
+    .y0(height * numOfPositiveBands) //TODO: change this to both Pos and Neg or something ???
     .interpolate("cardinal");
 
   function my(selection) {
 
     selection.each(function (d, i) {
       //Set the chart's dimensions
-      d3.select("#chart")
-        .attr("width", width * 2)
+      whereToDrawIt
+        .attr("width", width)
         .attr("height", height);
 
       //Draw the background for the chart
-      d3.select("#chart")
+      whereToDrawIt
         .insert("svg:rect")
           .attr("width", width)
           .attr("height", height)
           .style("fill", "#FFF");
 
       //Our canvas, where the curves will be rendered, and which will be clipped.
-      var chart = d3.select("#chart").append("svg:g");
+      chart = whereToDrawIt.append("svg:g");
 
       //Make the clipPath (for cropping the paths) //TODO: actually employ this
       chart.insert("defs")
@@ -63,11 +64,11 @@ function coolChart() {
           .attr("width", width)
           .attr("height", height); //height / 4 - 20);
 
-      //Make and render the curves.
+      //Make and render the Positive curves.
       chart.selectAll("path")
-          .data(d3.range(numOfBands)) //TODO: make this number of positive bands, then make another for negative ones.
+          .data(d3.range(numOfPositiveBands)) //TODO: make this number of positive bands, then make another for negative ones.
         .enter().append("path")
-          .attr("fill", "rgba(0, 0, 255, " + 1.0 / numOfBands + ")") //function (d, i) { return colors[i]; }) //TODO: use a non-linear scale for this instead!!!
+          .attr("fill", "rgba(0, 0, 255, " + 1.0 / numOfPositiveBands + ")") //function (d, i) { return colors[i]; }) //TODO: use a non-linear scale for this instead!!!
           .style("stroke-width", 2)
           .style("cursor", "help")
           .attr("d", d3area1(d))
@@ -75,7 +76,7 @@ function coolChart() {
 
 
       //Draw the outline for the chart
-      d3.select("#chart")
+      whereToDrawIt
         .append("svg:rect")
           .attr("width", width)
           .attr("height", height)
@@ -117,7 +118,25 @@ var data = [0, 5, 10, 7, 10, 0, 7, 8, 2.5];
 
 
 //var coolChart1 = coolChart().width(50).height(50);
-var coolChart1 = coolChart();
+var coolChart1 = coolChart(d3.select("#chart"));
 d3.select("#chart")
   .datum(data)
   .call(coolChart1);
+
+var data2 = [2, 3, 2, 2, 3, 1, 0, 1, 0];
+var coolChart2 = coolChart(d3.select("#chart2"));
+d3.select("#chart")
+  .datum(data2)
+  .call(coolChart2);
+
+var data3 = [0, 1, 0, 10, 0, 5, 2, 0, 2.5];
+var coolChart3 = coolChart(d3.select("#chart3"));
+d3.select("#chart")
+  .datum(data3)
+  .call(coolChart3);
+
+var data4 = [10, 8, 9, 5, 3, 7, 4, 8, 9, 6, 10];
+var coolChart4 = coolChart(d3.select("#chart4"));
+d3.select("#chart")
+  .datum(data4)
+  .call(coolChart4);
