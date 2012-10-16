10c9bcefa23dfa9ded2e9c54357372b22fb39839
Matthew Woelk <matt.woelk@gmail.com>
2012-10-16 17:22:19 -0500

    Beginnings of getting transitions working for "step-after" interpolations.

---
+++ ESGgirder_toy_data.js
@@ -0,0 +1,101 @@
+[
+	{
+		"ESGgirder1" : -47.8500
+	},
+	{
+		"ESGgirder1" : -39.3800
+	},
+	{
+		"ESGgirder1" : -39.3800
+	},
+	{
+		"ESGgirder1" : -44.9100
+	},
+	{
+		"ESGgirder1" : -44.9100
+	},
+	{
+		"ESGgirder1" : -42.0000
+	},
+	{
+		"ESGgirder1" : -42.0000
+	},
+	{
+		"ESGgirder1" : -44.2900
+	},
+	{
+		"ESGgirder1" : -44.2900
+	},
+	{
+		"ESGgirder1" : -48.1900
+	},
+	{
+		"ESGgirder1" : -48.1900
+	},
+	{
+		"ESGgirder1" : -39.4900
+	},
+	{
+		"ESGgirder1" : -39.4900
+	},
+	{
+		"ESGgirder1" : -48.1100
+	},
+	{
+		"ESGgirder1" : -48.1100
+	},
+	{
+		"ESGgirder1" : -41.0800
+	},
+	{
+		"ESGgirder1" : -41.0800
+	},
+	{
+		"ESGgirder1" : -45.1100
+	},
+	{
+		"ESGgirder1" : -45.1100
+	},
+	{
+		"ESGgirder1" : -50.3600
+	},
+	{
+		"ESGgirder1" : -50.3600
+	},
+	{
+		"ESGgirder1" : -39.5100
+	},
+	{
+		"ESGgirder1" : -39.5100
+	},
+	{
+		"ESGgirder1" : -45.7600
+	},
+	{
+		"ESGgirder1" : -45.7600
+	},
+	{
+		"ESGgirder1" : -41.1500
+	},
+	{
+		"ESGgirder1" : -41.1500
+	},
+	{
+		"ESGgirder1" : -43.1500
+	},
+	{
+		"ESGgirder1" : -43.1500
+	},
+	{
+		"ESGgirder1" : -48.5400
+	},
+	{
+		"ESGgirder1" : -48.5400
+	},
+	{
+		"ESGgirder1" : -40.2600
+	},
+	{
+		"ESGgirder1" : -40.2600
+	}
+]
