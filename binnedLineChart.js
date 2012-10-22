642d21c22ff859c8e73c259622d37b3d3ef219bc
MattWoelk <matt.woelk@gmail.com>
2012-10-22 12:56:48 -0500

    From sizes 4 and up transitions look okay. Smooth. Perhaps the start of something great. We shall see.

--- binnedLineChart.js
+++ binnedLineChart.js
@@ -6,6 +6,33 @@
 //      - mean won't work for quartiles. I checked.
 //      Fade based on how many pixels are being rendered.
 
+// This function is courtesy of http://bl.ocks.org/3916621
+// which comes originally from https://gist.github.com/3916621
+function pathTween(d1, precision, that) {
+  return function() {
+    var path0 = that
+    var path1 = path0.cloneNode(),
+        n0 = path0.getTotalLength(),
+        n1 = (path1.setAttribute("d", d1), path1).getTotalLength();
+
+    // Uniform sampling of distance based on specified precision.
+    var distances = [0], i = 0, dt = precision / Math.max(n0, n1);
+    while ((i += dt) < 1) distances.push(i);
+    distances.push(1);
+
+    // Compute point-interpolators at each distance.
+    var points = distances.map(function(t) {
+      var p0 = path0.getPointAtLength(t * n0),
+          p1 = path1.getPointAtLength(t * n1);
+      return d3.interpolate([p0.x, p0.y], [p1.x, p1.y]);
+    });
+
+    return function(t) {
+      return t < 1 ? "M" + points.map(function(p) { return p(t); }).join("L") : d1;
+    };
+  };
+}
+
 var binnedLineChart = function () {
   var outlinesOrNot = true;
 
@@ -370,17 +397,20 @@ var binnedLineChart = function () {
 
       //Make and render the Positive curves.
       currentSelection = paths.selectAll(".posPath")
-        .data(makeDataObjectForKeyFanciness(), function (d) {return d.type + d.which; });
+        .data(makeDataObjectForKeyFanciness(), function (d) {return d.type /*+ d.which*/; });
 
       //update
       currentSelection
         .transition().duration(500)
         .attr("opacity", function (d) { return binData.properties[d.type].opacity; }) // TODO: delete this line?
-        .attr("fill", function (d, i) { return "rgba(0,0,0,0)"; })
-        .style("stroke-width", function () { return outlinesOrNot ? 1 : 0; })
+        .attr("fill", "rgba(0,0,0,0)")
+        .style("stroke-width", outlinesOrNot ? 1 : 0)
         .style("stroke", function (d, i) { return binData.properties[d.type].colour; })
-        .attr("d", function (d, i) { return binData.levels[d.which][d.type + "d0"]; })
         .transition().duration(500)
+        .attrTween("d", function(d) {
+          //console.log(pathTween(binData.levels[d.which][d.type + "d0"], 4, this)()());
+          return     (pathTween(binData.levels[d.which][d.type + "d0"], 4, this)());
+        })
         .attr("transform", function (d, i) { return "translate(" + margins.left + ", 0)"; });
 
       //enter
@@ -534,3 +564,24 @@ var binnedLineChart = function () {
 
   return my;
 };
+
+
+
+//
+//var width = 960,
+//    height = 500;
+//
+//var svg = d3.select("body").append("svg")
+//    .attr("width", width)
+//    .attr("height", height);
+//
+//var d0 = "M0,0c100,0 0,100 100,100c100,0 0,-100 100,-100",
+//    d1 = "M0,0c100,0 0,100 100,100c100,0 0,-100 100,-100c100,0 0,100 100,100";
+//
+//svg.append("path")
+//    .attr("transform", "translate(180,150)scale(2,2)")
+//    .attr("fill", "rgba(0,0,0,0)")
+//    .attr("stroke", "#FF0")
+//    .attr("d", d0)
+//    .transition().duration(2000)
+//    .attrTween("d", pathTween(d1, 4));
