d0b0acc6daa63dbd700ba4b1b0511a0ff40fead3
Matt Woelk <matt.woelk@gmail.com>
2012-12-10 18:17:34 -0600

    All graphs are on the same svg. Not elegant, yet.

--- bridgecharts.js
+++ bridgecharts.js
@@ -1,10 +1,14 @@
 var plots = []; //an array of all plots
+console.log("useragent: " + navigator.userAgent);
 
 var supportsOrientationChange = "onorientationchange" in window,
     orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
 window.addEventListener(orientationEvent, function() {
-  setTimeout("redraw()", 500); //TODO: make only wait for Android
-  //redraw();
+  if (navigator.userAgent.match(/android/i)) {
+    setTimeout("redraw()", 500); //Only wait for Android
+  } else {
+    redraw();
+  }
 }, false);
 
 var redraw = function () {
@@ -44,27 +48,40 @@ socket.on('news', function (data) {
 
   var plot10 = binnedLineChart();
 
-  var pl10 = d3.select("#charts").append("svg").datum(_.map(json, function (d) { return -d.ESGgirder1; })).call(plot10);
+  var pl10 = d3.select("#charts").append("g").datum(_.map(json, function (d) { return -d.ESGgirder1; })).call(plot10);
 
   var plot11 = binnedLineChart();
 
-  var pl11 = d3.select("#charts").append("svg").datum(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; })).call(plot11);
+  var pl11 = d3.select("#charts").append("g").datum(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; })).call(plot11);
 
   var plot12 = binnedLineChart();
 
-  var pl12 = d3.select("#charts").append("svg").datum(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; })).call(plot12);
+  var pl12 = d3.select("#charts").append("g").datum(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; })).call(plot12);
 
   var plot13 = binnedLineChart();
 
-  var pl13 = d3.select("#charts").append("svg").datum(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; })).call(plot13);
+  var pl13 = d3.select("#charts").append("g").datum(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; })).call(plot13);
 
-  plot10.offsetWidth(document.getElementById("charts").offsetWidth).height(75).update();
-  plot11.offsetWidth(document.getElementById("charts").offsetWidth).height(75).update();
-  plot12.offsetWidth(document.getElementById("charts").offsetWidth).height(75).update();
-  plot13.offsetWidth(document.getElementById("charts").offsetWidth).height(75).update();
+  plot10.offsetWidth(document.getElementById("charts").offsetWidth).height(75).margin_top(10).update();
+  plot11.offsetWidth(document.getElementById("charts").offsetWidth).height(75).margin_top(120*1 + 10).update();
+  plot12.offsetWidth(document.getElementById("charts").offsetWidth).height(75).margin_top(120*2 + 10).update();
+  plot13.offsetWidth(document.getElementById("charts").offsetWidth).height(75).margin_top(120*3 + 10).update();
   plots.push(plot10);
   plots.push(plot11);
   plots.push(plot12);
   plots.push(plot13);
   redraw();
 });
+
+
+//TODO: ongoing. get zoom to zoom ALL graphs
+var zoom = d3.behavior.zoom()
+  .on("zoom", zoom);
+
+//chart.call(d3.behavior.zoom().x(xScale).y(yScale).scaleExtent([0.125, 8]).on("zoom", my.zoom));
+//
+//var redraw = function () {
+//  plots.forEach(function (plt) {
+//    plt.offsetWidth(document.getElementById("charts").offsetWidth).update();
+//  });
+//}
