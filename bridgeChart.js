4f72b4a06e1de59cad8b446e88525b32399eec37
Matt Woelk <matt.woelk@gmail.com>
2013-01-09 11:07:13 -0600

    Changed viewport setting for pixel-perfect mobile viewing.

--- bridgeChart.js
+++ bridgeChart.js
@@ -1,5 +1,6 @@
 // TODO:
 // BUGS AND IMPROVEMENTS:
+//      Update when click-up on the slider, to decrease the amount of rendering.
 //      When going from a large window to a small window, some background elements are still being rendered as being very large (so a horizontal scroll bar appears).
 //      When rendering on chrome, sometimes the new data arrives before the old one is rendered. This should be synchronous somehow instead so that they don't both render and overlap each other.
 //      [ TODO CURRENT TASK!!!!  ]    Make the levels-calculating dynamic; as needed
