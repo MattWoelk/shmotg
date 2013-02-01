6e9dea395d6a57ccd4ffd69d560062d297f23077
Matt Woelk <matt.woelk@gmail.com>
2013-02-01 09:33:22 -0600

    simplified plot creation

--- bridgecharts.js
+++ bridgecharts.js
@@ -171,45 +171,8 @@ socket.on('news', function (data) {
   var json = JSON.parse(data);
   socket.emit('ack', "Message received!");
 
-  var plot10 = binnedLineChart(_.map(json, function (d) { return -d.ESGgirder1; }));
-  plot10.xScale(copyScale(xScale));
-
-  var pl10 = d3.select("#charts").append("g").call(plot10);
-
-  var plot11 = binnedLineChart(_.map(json,function (d) { return Math.random() * 5 + -d.ESGgirder1; }));
-  plot11.xScale(copyScale(xScale));
-
-  var pl11 = d3.select("#charts").append("g").call(plot11);
-
-  // TODO: make the marginTop values dynamic
-  plot10.containerWidth(document.getElementById("chartContainer").offsetWidth).height(75).marginTop(10).update();
-  plot11.containerWidth(document.getElementById("chartContainer").offsetWidth).height(75).marginTop(120*1 + 10).update();
-  plots.push(plot10);
-  plots.push(plot11);
-  redraw();
-
-  d3.select("#charts").attr("height", 120*plots.length).attr("width", document.getElementById("chartContainer").offsetWidth);
-
-  zoomSVG.attr("width", document.getElementById("chartContainer").offsetWidth)
-          .attr("height", document.getElementById("chartContainer").offsetHeight)
-          .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-
-  zoomRect.attr("width", document.getElementById("chartContainer").offsetWidth - margin.left - margin.right)
-    .attr("height", document.getElementById("chartContainer").offsetHeight - margin.top)
-    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-
-  zoomRect.attr("fill", "rgba(0,0,0,0)")
-    .call(zoom);
-
-  //redefine this function now that we have data for it to work from
-  updateZoom = function () {
-    xScale = plot10.xScale();
-    yScale = plot10.yScale();
-    zoom.x(xScale);
-    zoom.y(yScale);
-  };
-
-  updateZoom();
+  initPlot(json);
+  initPlot(_.map(json, function (d) { return { ESGgirder18: Math.random() * 5 + d.ESGgirder18, SampleIndex: d.SampleIndex}; }));
 });
 
 
@@ -217,7 +180,6 @@ socket.on('news', function (data) {
 // A demonstration with example data in case the server is down:
 
 // wait 2 seconds to give the server a chance to send the data (to avoid the demo popping up and then disappearing)
-// TODO: set this back to 2000 or so.
 setTimeout(rundemo, 1500);
 
 function rundemo() {
@@ -228,40 +190,42 @@ function rundemo() {
 
     var json = data;
 
-    //var plotD = binnedLineChart(_.map(json, function (d) { return -d.ESGgirder1; }));
-    var plotD = binnedLineChart(json);
-    plotD.xScale(copyScale(xScale));
-
-    var pl10 = d3.select("#charts").append("g").call(plotD);
-
-    plotD.containerWidth(document.getElementById("chartContainer").offsetWidth).height(75).marginTop(10).update();
-
-    plots.push(plotD);
-
-    // TODO: trim this all; put it into a separate function, so both this and the from-server code run the same identical code.
-    redraw();
-
-    d3.select("#charts").attr("height", 120*plots.length).attr("width", document.getElementById("chartContainer").offsetWidth); //TODO: make this dynamic
-
-    zoomSVG.attr("width", document.getElementById("chartContainer").offsetWidth)
-            .attr("height", document.getElementById("chartContainer").offsetHeight)
-            .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-
-    zoomRect.attr("width", document.getElementById("chartContainer").offsetWidth - margin.left - margin.right)
-      .attr("height", document.getElementById("chartContainer").offsetHeight - margin.top)
-      .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-
-    zoomRect.attr("fill", "rgba(0,0,0,0)")
-      .call(zoom);
-
-    // TODO: redefine this function now that we have data for it to work from
-    updateZoom = function () {
-      xScale = plotD.xScale();
-      yScale = plotD.yScale();
-      zoom.x(xScale);
-      zoom.y(yScale);
-    };
-
-    updateZoom();
+    initPlot(json);
   });
+}
+
+function initPlot(data) {
+  var plot = binnedLineChart(data);
+  plot.xScale(copyScale(xScale));
+
+  var pl = d3.select("#charts").append("g").call(plot);
+
+  plot.containerWidth(document.getElementById("chartContainer").offsetWidth).height(75).marginTop(10).update();
+
+  plots.push(plot);
+
+  redraw();
+
+  d3.select("#charts").attr("height", 120*plots.length).attr("width", document.getElementById("chartContainer").offsetWidth); //TODO: make this dynamic
+
+  zoomSVG.attr("width", document.getElementById("chartContainer").offsetWidth)
+         .attr("height", document.getElementById("chartContainer").offsetHeight)
+         .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+  zoomRect.attr("width", document.getElementById("chartContainer").offsetWidth - margin.left - margin.right)
+          .attr("height", document.getElementById("chartContainer").offsetHeight - margin.top)
+          .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+  zoomRect.attr("fill", "rgba(0,0,0,0)")
+          .call(zoom);
+
+  // Redefine this function now that we have data for it to work from
+  updateZoom = function () {
+    xScale = plot.xScale();
+    yScale = plot.yScale();
+    zoom.x(xScale);
+    zoom.y(yScale);
+  };
+
+  updateZoom();
 }
