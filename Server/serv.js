67e36100047f7c5f0712d2002b9f8ea660f4e981
Matt Woelk <matt@mattwoelk.ca>
2013-05-07 14:34:41 -0500

    problem: no bins should be missing in this range

--- Server/serv.js
+++ Server/serv.js
@@ -40,11 +40,14 @@ try {
     binData.replaceBinnedData(datDat, i, true);
   }
   console.log("all has been read");
+  console.log("   ", binData.getMaxRawMS());
+  console.log("   ", binData.getMinRawMS());
+  console.log(binData.missingBins([1325567546364, 1325567564076], 0))
 } catch (err) {
   console.log(err);
   throw err;
 }
-
+/*
 //var app = http.createServer(); //(handler); //if we want to serve html, too. // for html
 var io = require('socket.io').listen(8080); //(app) for html
 io.configure(function () { io.set('log level', 2); });
