1a394a342e56995206e17e80f73daacc1a839f19
Matt Woelk <matt@mattwoelk.ca>
2013-06-17 15:06:56 -0500

    temporary disabling of gathering new database data

--- Server/serv.js
+++ Server/serv.js
@@ -23,6 +23,7 @@ function dt (num) {
 // {{{ GLOBAL VARIABLES
 var binData = binnedData();
 var READ_OLD_DATA = true;
+var READ_NEW_DATA = false;
 // GLOBAL VARIABLES}}}
 
 if (READ_OLD_DATA) {
@@ -190,7 +191,7 @@ io.sockets.on('connection', function (socket) {
         // SEND TO CLIENT }}}
 
         // See if we need to get data from the database (because the level is lower than we have pre-binned)
-        if (req.bin_level < 6) { // TODO: magic
+        if (READ_OLD_DATA && req.bin_level < 6) { // TODO: magic
             // {{{ CREATE REQUEST
             console.log("** LOW LEVEL: GET FROM DATABASE **", req.bin_level);
 
