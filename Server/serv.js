552ba1f91b213f54aa9a39b88ee0c82f75e9086b
Matt Woelk <matt@mattwoelk.ca>
2013-06-26 13:58:10 -0500

    the beginnings of couchdb with cradle

--- Server/serv.js
+++ Server/serv.js
@@ -3,9 +3,70 @@ var http = require('http');
 var fs = require('fs');
 var mysql = require('mysql');
 _ = require('underscore');
-d3 = require("d3");
 require("../binnedData.js");
 require("./database.js");
+
+var cradle = require('cradle')
+var db = new(cradle.Connection)().database('starwars');
+
+db.save('skywalker', { force: 'light', name: 'Luke Skywalker' }, function (err, res) {
+    if (err) {
+        //Handle error
+    } else {
+        // Handle success
+        console.log(res);
+    }
+});
+
+db.save('vader', { force: 'dark', name: 'Darth Vader' }, function (err, res) {
+    if (err) {
+        //Handle error
+    } else {
+        // Handle success
+        console.log(res);
+    }
+});
+
+db.get('skywalker', function (err, doc) {
+    doc.name; // 'Darth Vader'
+    console.log(doc.force, '== dark');
+});
+
+// THIS WORKS!
+db.temporaryView({
+    map: function (doc) {
+        if (doc.force) emit(doc._id, doc);
+    }
+}, function (err, res) {
+    if (err) console.log(err);
+    console.log(res);
+})
+
+// THIS doesn't work... :C
+//db.save('_design/characters', {
+//    all: {
+//        map: function (doc) {
+//            if (doc.name) emit(doc.name, doc);
+//        }
+//    },
+//    darkside: {
+//        map: function (doc) {
+//            if (doc.name && doc.force == 'dark') {
+//                emit(null, doc);
+//            }
+//        }
+//    }
+//});
+//
+//db.view('_design/characters/all', {group: true, reduce: true }, function (err, res) {
+//    if (err) {
+//        console.log("_design/characters/all", err);
+//        return;
+//    }
+//    res.forEach(function(row) {
+//        console.log("%s is on the %s side of the force.", row.name, row.force);
+//    });
+//});
 
 red = '\033[31m';
 yellow = '\033[33m';
@@ -22,7 +83,7 @@ function dt (num) {
 
 // {{{ GLOBAL VARIABLES
 var binData = binnedData();
-var READ_OLD_DATA = true;
+var READ_OLD_DATA = false;
 var READ_NEW_DATA = true;
 // GLOBAL VARIABLES}}}
 
@@ -111,17 +172,6 @@ function dateStringToMilliseconds (dateStr) {
     return d3.time.format("%a %b %d %Y %H:%M:%S")
     .parse(dateStr.substring(0, 24))
     .getTime();
-}
-
-function samplesToMilliseconds (sampleIndex) {
-    var samplesPerSecond = 200;
-    var msPerSample = 1000/samplesPerSecond;
-    var mils = sampleIndex * msPerSample;
-    return mils;
-}
-
-function dateAndSampleIndexStringToMilliseconds (dateStr, sampleIndex) {
-    return dateStringToMilliseconds(dateStr) + samplesToMilliseconds(sampleIndex);
 }
 
 function pad(integ) {
