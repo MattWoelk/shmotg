ea19826dec56e086632f0824d4e0f6aaf5ad7c52
Matt Woelk <matt@mattwoelk.ca>
2013-06-26 15:21:43 -0500

    Testing successes.

--- Server/couchdbtest.js
+++ Server/couchdbtest.js
@@ -1,12 +1,13 @@
 var cradle = require('cradle')
 var db = new(cradle.Connection)().database('starwars');
 
+// THIS worked.
 db.save('skywalker', { force: 'light', name: 'Luke Skywalker' }, function (err, res) {
     if (err) {
         //Handle error
     } else {
         // Handle success
-        console.log(res);
+        //console.log(res);
     }
 });
 
@@ -15,47 +16,62 @@ db.save('vader', { force: 'dark', name: 'Darth Vader' }, function (err, res) {
         //Handle error
     } else {
         // Handle success
-        console.log(res);
+        //console.log(res);
     }
 });
 
+// THIS WORKS! (Will try to do things this way)
 db.get('skywalker', function (err, doc) {
     doc.name; // 'Darth Vader'
     console.log(doc.force, '== dark');
 });
 
 // THIS WORKS!
-db.temporaryView({
-    map: function (doc) {
-        if (doc.force) emit(doc._id, doc);
-    }
-}, function (err, res) {
-    if (err) console.log(err);
-    console.log(res);
-})
+//db.temporaryView({
+//    map: function (doc) {
+//        if (doc.force) emit(doc._id, doc);
+//    }
+//}, function (err, res) {
+//    if (err) console.log(err);
+//    console.log(res);
+//})
 
-// THIS doesn't work... :C
-//db.save('_design/characters', {
-//    all: {
-//        map: function (doc) {
-//            if (doc.name) emit(doc.name, doc);
-//        }
-//    },
-//    darkside: {
-//        map: function (doc) {
-//            if (doc.name && doc.force == 'dark') {
-//                emit(null, doc);
-//            }
+//// THIS WORKS!
+//db.save('_design/force', {
+//    views: {
+//        forcely: {
+//            map: 'function (doc) { if (doc.force) emit(doc.force, doc); }'
 //        }
 //    }
 //});
 //
-//db.view('_design/characters/all', {group: true, reduce: true }, function (err, res) {
+//// THIS WORKS!
+//db.view('force/forcely', function (err, res) {
+//    if (err) console.log("err", err);
+//    res.forEach(function (row) {
+//        console.log(row.force);
+//    })
+//});
+
+// THIS WORKS!
+//db.view('characters/all', function (err, res) {
 //    if (err) {
-//        console.log("_design/characters/all", err);
+//        console.log("characters/all", err);
 //        return;
 //    }
 //    res.forEach(function(row) {
 //        console.log("%s is on the %s side of the force.", row.name, row.force);
 //    });
+//});
+
+//db.save('_design/user', {
+//    views: {
+//        byUsername: {
+//            map: 'function (doc) { if (doc.resource === "User") { emit(doc.username, doc) } }'
+//        }
+//    }
+//});
+//
+//db.view('user/byUsername', { key: 'luke' }, function (err, doc) {
+//    console.log("_design/user", err, doc, "_design/user");
 //});
